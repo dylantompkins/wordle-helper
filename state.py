aa1e4e8689fb183076a257556d44ee377596966f
Dylan Tompkins <dylanltompkins@hotmail.com>
2022-02-04 21:47:49 -0800

    commit

--- state.py
+++ state.py
@@ -4,3 +4,13 @@ class State:
 
     def remove_letter(self, letter):
         self.words = [word for word in self.words[:] if not letter in word]
+
+    def correct_pos(self, letter, pos):
+        self.words = [word for word in self.words[:] if letter == word[pos]]
+
+    def wrong_pos(self, letter, pos):
+        # only words containing the letter
+        self.words = [word for word in self.words[:] if letter in word]
+        
+        # eliminate the words that have letter at pos
+        self.words = [word for word in self.words[:] if not letter == word[pos]]
